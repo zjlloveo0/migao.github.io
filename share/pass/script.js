b8eb3a0eff2a8face413122e627e8772d2e39857
zjlloveo0 <1357423913@qq.com>
2020-02-07 15:55:13 +0800

    Revert "1"
    This reverts commit 2089d5194cec2410c83c05e2966b2ab0f155f7a6.

--- share/pass/script.js
+++
@@ -1,7 +0,0 @@
-const countDownWrapper = document.getElementById('js-count-down');
-countDownWrapper.addEventListener('click', ev => {
-  countDownWrapper.style.display = 'none';
-  setTimeout(() => {
-    countDownWrapper.style.display = 'block';
-  }, 0);
-});
